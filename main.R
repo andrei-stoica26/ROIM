e7b5532f95dc7ecbe0977733f1e670c989e92fe9
andrei-stoica26 <andreistoica@foxmail.com>
2026-03-27 12:21:20 +0800

    Exploratory analyses

--- main.R
+++ main.R
@@ -76,29 +76,59 @@ p <- DimPlot(miniSeurat, group.by='orig.ident', label = TRUE, label.size=3) + No
 miniSeurat <- FindNeighbors(miniSeurat, reduction = "umap", dims = 1:2)
 miniSeurat <- FindClusters(miniSeurat, resolution=0.1)
 
+miniSeurat <- subset(miniSeurat, seurat_clusters != 3)
+miniSeurat <- processSeurat(miniSeurat, minFeatCells=1, cutoff=0.005)
+p <- DimPlot(miniSeurat, group.by='orig.ident')
+devPlot(p)
+
 a <- FindMarkers(miniSeurat, ident.1=3, only.pos=TRUE, 
                  min.diff.pct=0.3, 
                  logfc.threshold=2)
 
+Idents(seuratObj) <- 'celltype'
+genes <- rownames(a)[seq(4)]
+
+invisible(lapply(genes, function(gene){
+    p <- FeaturePlot(miniSeurat, gene)
+    devPlot(p)
+    p <- FeaturePlot(seuratObj, gene, label=TRUE, repel=TRUE, label.size=3)
+    devPlot(p)
+}))
+
 m <- genesER(rownames(a), 'human')
+p <- newCnetplot(m)
+devPlot(p)
 
-cnetplot(m, color_category='purple', color_item='red', color_edge='thistle') + NoLegend()
+a <- FindMarkers(miniSeurat, ident.1=c(0, 1, 2), only.pos=TRUE, 
+                 min.diff.pct=0.2, 
+                 logfc.threshold=0.1)
 
-enrichplot::ccnetplot.enrichResultenrichplot::cnetplot(m)
+p <- VlnPlot(miniSeurat, 'nCount_RNA')
+devPlot(p)
+p <- VlnPlot(miniSeurat, 'nFeature_RNA')
+devPlot(p)
 
-newCnetplot(m)
-library(org.Hs.eg.db)
-library(clusterProfiler)
-entrezGenes <- AnnotationDbi::select(org.Hs.eg.db,
-                                     keys=rownames(a),
-                                     columns=c("ENTREZID", "SYMBOL"),
-                                     keytype="SYMBOL")[[2]]
+p <- VlnPlot(seuratObj, 'nCount_RNA', group.by='celltype') + NoLegend()
+devPlot(p)
+p <- VlnPlot(seuratObj, 'nFeature_RNA', , group.by='celltype') + NoLegend()
+devPlot(p)
 
-v <- enrichGO(entrezGenes, OrgDb='org.Hs.eg.db')
-cnetplot(v)
 
-BiocManager::install("clusterProfiler")
+################################################################################
 
+DimPlot(miniSeurat, label=TRUE)
+cells <- colnames(subset(miniSeurat, seurat_clusters != 3))
+
+seuratObj <- qs_read('annotatedSeurat.qs2')
+
+miniSeurat <- subset(seuratObj, cells=cells)
+miniSeurat <- removeRareFeatures(miniSeurat, 1)
+miniSeurat <- removeRareFeatures(miniSeurat, 1, 'ATAC')
+miniSeurat <- basicDimRed(miniSeurat)
+miniSeurat <- jointUMAP(miniSeurat, FALSE, cutoff=0.005)
+
+p <- DimPlot(miniSeurat, label=TRUE, group.by='orig.ident')
+devPlot(p)
 
 ################################################################################
     
@@ -141,5 +171,3 @@ p1 <- featurePlot(miniSeurat, 'Lineage1',
     geom_segment(data=df, aes(x=x, y=y, xend=xEnd, yend=yEnd),
                  arrow = arrow(length = unit(0.1, "cm")))
 devPlot(p1)
-
-
